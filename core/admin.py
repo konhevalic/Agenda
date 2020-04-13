bd4590d00d16d4515bb883526799541ab65de484
Alan Konhevalic <alan.patrick.konhevalic@gmail.com>
2020-04-13 17:03:12 -0300

    criado tela de listagem de eventos

--- core/admin.py
+++ core/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from core.models import Evento
+
+
 # Register your models here.
 
 
 class EventoAdmin(admin.ModelAdmin):
     list_display = ('titulo', 'data_evento', 'data_criacao')
     list_filter = ('titulo',)
+
 
 admin.site.register(Evento, EventoAdmin)
